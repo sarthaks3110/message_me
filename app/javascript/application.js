e502dac922bbef4b974042210def036e9cb3fb19
sarthaks3110 <sarthakbhosalev9@gmail.com>
2026-05-07 03:43:52 +0530

    added menu then dropdownlist and then change layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require jquery
+//= require jquery_ujs
+//= require semantic-ui
+
+$(document).ready(function() {
+  $('.ui.dropdown').dropdown();
+});
